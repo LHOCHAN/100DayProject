538a8a18fa7f743fc257ff3e69fc7c3991044abd
gongjiwon <rag9008@naver.com>
2019-03-05 21:33:36 +0900

    create upload camera photo view controller, create upload camera photo storyboard, add image assets, apply xUnique to project file

---
+++ 100DayProject/UploadTaskCamera/UploadCameraPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadCameraPhotoViewController.swift
+//  100DayProject
+//
+//  Created by 공지원 on 05/03/2019.
+//  Copyright © 2019 100DayTeam. All rights reserved.
+//
+
+import UIKit
+
+class UploadCameraPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
